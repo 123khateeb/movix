1a5499b17b6b8c2dc149d3351809493c9a315548
Khateeb Ahmad <khateebahmad69@gmail.com>
2023-12-20 17:24:22 +0530

    movix app code completed

---
+++ src/pages/home/popular/Popular.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Carousel from "../../../Components/carousel/Carousel";
+import ContentWrapper from "../../../Components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../Components/switchTab/SwitchTabs";
+import UseFetch from "../../../hooks/useFetch";
+
+const Popular = () => {
+  const [endpoint, setEndpoint] = useState("movie");
+  const { data, loading } = UseFetch(`/${endpoint}/popular`);
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">What's Popular</span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel  
+            data={data?.results} 
+            loading={loading} 
+            endpoint={endpoint} />
+    </div>
+  );
+};
+
+export default Popular;
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Carousel from "../../../Components/carousel/Carousel";
+import ContentWrapper from "../../../Components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../Components/switchTab/SwitchTabs";
+import UseFetch from "../../../hooks/useFetch";
+
+const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = UseFetch(`/trending/all/${endpoint}`);
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
+    </div>
+  );
+};
+
+export default Trending;
